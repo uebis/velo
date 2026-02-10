853939d7a30fb9334f3117a223e4ec49636f1255
uebis <qa.andrezza+github@gmail.com>
2026-02-10 04:58:25 -0300

    AAA (Arrange, Act, Assert)

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,21 +1,21 @@
 import { test, expect } from '@playwright/test'
 
-test('deve consultar um pedido aprovado', async ({ page }) => {
-  await page.goto('http://localhost:5173/')
+// AAA (Arrange, Act, Assert)
 
-  //checkpoint: deve estar na hero page
+test('deve consultar um pedido aprovado', async ({ page }) => {
+  // Arrange
+  await page.goto('http://localhost:5173/')
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
 
   await page.getByRole('link', { name: 'Consultar Pedido' }).click()
-
-  //checkpoint: deve estar na página de consulta de pedido
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
 
+  // Act
   await page.getByTestId('search-order-id').click()
   await page.getByTestId('search-order-id').fill('VLO-34UAZI')
   await page.getByTestId('search-order-button').click()
 
-  //checkpoint: número de pedido e status aparecem
+  // Assert
   await expect(page.getByTestId('order-result-id')).toBeVisible()
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-34UAZI')
   await expect(page.getByTestId('order-result-status')).toBeVisible()
