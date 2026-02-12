9246460e59e11760e6ee51a206e5c1af9b519473
uebis <qa.andrezza+github@gmail.com>
2026-02-12 02:54:26 -0300

    DESAFIO: Sobreviva sem Data Test IDs

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -3,22 +3,28 @@ import { test, expect } from '@playwright/test'
 // AAA (Arrange, Act, Assert)
 
 test('deve consultar um pedido aprovado', async ({ page }) => {
+
   // Arrange
   await page.goto('http://localhost:5173/')
+
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
 
   await page.getByRole('link', { name: 'Consultar Pedido' }).click()
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
 
   // Act
-  await page.getByTestId('search-order-id').click()
-  await page.getByTestId('search-order-id').fill('VLO-34UAZI')
-  await page.getByTestId('search-order-button').click()
+  await page.getByRole('textbox', { name: 'Número do Pedido' }).fill('VLO-34UAZI')
+
+  await page.getByRole('button', { name: 'Buscar Pedido' }).click()
 
   // Assert
-  await expect(page.getByTestId('order-result-id')).toBeVisible()
-  await expect(page.getByTestId('order-result-id')).toContainText('VLO-34UAZI')
-  await expect(page.getByTestId('order-result-status')).toBeVisible()
-  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
+
+  // DESAFIO: Sobreviva sem Data Test IDs
+  // Optei por usar xPath simulando o cenário em que eu não tenho acesso ao código para adicionar algum id ao parágrafo.
+  // Como eu já valido pelo xPath, usando o texto como localizador, não tem a necessidade de usar a validação .toContainText()
+
+  await expect(page.locator('//p[text()="Pedido"]/..//p[text()="VLO-34UAZI"]')).toBeVisible()
+
+  await expect(page.locator('//div[text()="APROVADO"]')).toBeVisible()
 
 })
